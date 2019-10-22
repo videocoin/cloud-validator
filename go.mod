e9e4ea78132d9e73d232e3de03b78a564b81ecdf
Nikolay Stoev <nikolay@liveplanet.net>
2019-10-22 12:11:41 -0700

    vendor

--- go.mod
+++ go.mod
@@ -5,14 +5,19 @@ go 1.12
 require (
 	github.com/cespare/cp v1.1.1 // indirect
 	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
+	github.com/corona10/goimagehash v1.0.1
+	github.com/dwbuiten/go-mediainfo v0.0.0-20150630175133-91f51f40c56a
 	github.com/edsrzf/mmap-go v1.0.0 // indirect
 	github.com/ethereum/go-ethereum v1.8.27
 	github.com/fjl/memsize v0.0.0-20190710130421-bcb5799ab5e5 // indirect
 	github.com/gogo/protobuf v1.3.0
+	github.com/google/uuid v1.0.0
+	github.com/hashicorp/golang-lru v0.5.3 // indirect
 	github.com/huin/goupnp v1.0.0 // indirect
 	github.com/jackpal/go-nat-pmp v1.0.1 // indirect
 	github.com/karalabe/hid v1.0.0 // indirect
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/opentracing/opentracing-go v1.1.0
 	github.com/sirupsen/logrus v1.4.2
 	github.com/uber-go/atomic v1.4.0 // indirect
---
+++ vendor/github.com/dwbuiten/go-mediainfo/mediainfo/mediainfo.go
@@ -0,0 +1,84 @@
+package mediainfo
+
+/*
+ #cgo linux LDFLAGS: -ldl
+ #cgo darwin LDFLAGS: -framework CoreFoundation
+ #include <stdlib.h>
+ #include "c/mediainfo_wrapper.c"
+*/
+import "C"
+
+import (
+    "errors"
+    "unsafe"
+)
+
+const (
+    General = C.MediaInfo_Stream_General
+    Video   = C.MediaInfo_Stream_Video
+    Audio   = C.MediaInfo_Stream_Audio
+    Image   = C.MediaInfo_Stream_Image
+)
+
+/* Dont expose this ugliness. */
+type MediaInfo struct {
+    ptr unsafe.Pointer
+}
+
+/* Loas the shared library. */
+func Init() {
+    C.mediainfo_c_init()
+}
+
+/*
+ * Opens and parses the file.
+ *
+ * Takes a full path or reltaive path as an argument,
+ * and returns a MediaInfo handler.
+ */
+func Open(file string) (MediaInfo, error) {
+    var ret MediaInfo
+
+    cfile := C.CString(file)
+    defer C.free(unsafe.Pointer(cfile))
+
+    cptr   := C.mediainfo_c_open(cfile)
+    ret.ptr = cptr
+    if cptr == nil {
+        return ret, errors.New("Cannot open file.")
+    }
+
+    return ret, nil
+}
+
+/*
+ * Get audio or video info for a key.
+ *
+ * Matches up with the list available via:
+ *     mediainfo --Info-Parameters
+ *
+ * Only handles one video and audio stream currently.
+ *
+ * Takes a key, a stream number, and a stream type as 
+ * arguments.
+ */
+func (handle MediaInfo) Get(key string, stream int, typ uint32) (string, error) {
+    ckey  := C.CString(key)
+    cptr  := unsafe.Pointer(handle.ptr)
+    defer C.free(unsafe.Pointer(ckey))
+
+    cret := C.mediainfo_c_get(cptr, ckey, C.size_t(stream), typ)
+    ret  := C.GoString(cret)
+    if len(ret) == 0 {
+        return "", errors.New("Cannot get value for key.")
+    }
+
+    return ret, nil
+}
+
+/* Close a handle. */
+func (handle MediaInfo) Close() {
+    cptr  := unsafe.Pointer(handle.ptr)
+
+    C.mediainfo_c_close(cptr)
+}
