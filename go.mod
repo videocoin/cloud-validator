538bf8de2193e56fff01fd7789c58c20f2535dce
Nikita Savik <nikita@liveplanet.net>
2020-05-25 16:14:28 +0300

    Update deps

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/corona10/goimagehash v1.0.1
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/google/uuid v1.0.0
+	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/opentracing/opentracing-go v1.1.0
@@ -14,7 +16,7 @@ require (
 	github.com/vansante/go-ffprobe v1.1.0
 	github.com/videocoin/cloud-api v0.2.14
 	github.com/videocoin/cloud-pkg v0.0.6
-	google.golang.org/grpc v1.23.1
+	google.golang.org/grpc v1.27.1
 )
 
 replace github.com/videocoin/cloud-api => ../cloud-api
---
+++ vendor/github.com/videocoin/cloud-pkg/logger/logrus.go
@@ -0,0 +1,73 @@
+package logger
+
+import (
+	"os"
+	"time"
+
+	logrussentry "github.com/evalphobia/logrus_sentry"
+	logrusloki "github.com/schoentoon/logrus-loki"
+	"github.com/sirupsen/logrus"
+)
+
+func NewLogrusLogger(serviceName string, serviceVersion string, lokiURL *string) *logrus.Entry {
+	l := logrus.New()
+
+	loglevel = os.Getenv("LOGLEVEL")
+	if loglevel == "" {
+		loglevel = logrus.InfoLevel.String()
+	}
+	level, err := logrus.ParseLevel(loglevel)
+	if err != nil {
+		level = logrus.InfoLevel
+	}
+
+	l.SetLevel(level)
+	l.SetFormatter(&logrus.JSONFormatter{TimestampFormat: time.RFC3339Nano})
+
+	sentryDSN = os.Getenv("SENTRY_DSN")
+	if sentryDSN != "" {
+		sentryLevels := []logrus.Level{
+			logrus.PanicLevel,
+			logrus.FatalLevel,
+			logrus.ErrorLevel,
+		}
+		sentryTags := map[string]string{
+			"service": serviceName,
+			"version": serviceVersion,
+		}
+		sentryHook, err := logrussentry.NewAsyncWithTagsSentryHook(
+			sentryDSN,
+			sentryTags,
+			sentryLevels,
+		)
+		sentryHook.StacktraceConfiguration.Enable = true
+		sentryHook.Timeout = 5 * time.Second
+		sentryHook.SetRelease(serviceVersion)
+
+		if err != nil {
+			l.Warning(err)
+		} else {
+			l.AddHook(sentryHook)
+		}
+	}
+
+	if lokiURL != nil && *lokiURL != "" {
+		lokiHook, err := logrusloki.NewLoki(*lokiURL, 1024, 2)
+		lokiHook.AddData("app", serviceName)
+		lokiHook.AddData("version", serviceVersion)
+		if err != nil {
+			l.Warning(err)
+		} else {
+			l.AddHook(lokiHook)
+		}
+	}
+
+	logger := logrus.
+		NewEntry(l).
+		WithFields(logrus.Fields{
+			"service": serviceName,
+			"version": serviceVersion,
+		})
+
+	return logger
+}
